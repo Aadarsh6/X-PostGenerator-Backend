ce3bc70420e13467b944da24f664df66da05ce46
Adarsh <140302686+Aadarsh6@users.noreply.github.com>
2026-06-24 23:10:45 +0530

    -m

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,9 +5,7 @@ export const corsOptions: cors.CorsOptions = {
     'https://xcraft.aadarshm.me/',
     'https://x-post-generator-ruby.vercel.app',
     "http://localhost:5175/dashboard",
-    'http://localhost:3000',
     'http://localhost:5173',
-    'http://localhost:5175'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
